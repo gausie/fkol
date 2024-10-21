0e04cc39f118248de26d515e4d2130469c1562a0
Sam Gaus <sam@gaus.co.uk>
2024-10-21 12:45:58 +0100

    Format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
@@ -9,8 +9,8 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": ["error", { "caughtErrors": "none" }],
-    }
+      "@typescript-eslint/no-unused-vars": ["error", { caughtErrors: "none" }],
+    },
   },
   {
     plugins: {
